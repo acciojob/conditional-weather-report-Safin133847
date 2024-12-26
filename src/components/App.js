be67ef0cc7bf12477d5fd9846537d61160463ff3
Safin133847 <157776203+Safin133847@users.noreply.github.com>
2024-12-26 18:48:06 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,15 @@ import React from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [weather, setWeather] = useState({ temperature: 25, conditions: "Sunny" });
+  const tempColor = weather.temperature > 20 ? "rgb(255, 0, 0)" : "rgb(0, 0, 255)";
   return (
     <div>
         {/* Do not remove the main div */}
+        <div className="weather-display">
+        <p style={{ color: tempColor }}>Temperture: {weather.temperature}°C</p>
+        <span style={{ color: tempColor }}>Conditions: {weather.conditions}</span>
+        </div>
     </div>
   )
 }
